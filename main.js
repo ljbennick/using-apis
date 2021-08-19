931871123472c546c1d6e0effa343d8833f9571d
Jaxon Bennick <ljbennick5@gmail.com>
2021-08-19 02:31:34 -0600

    Accessing api's and working with axios and endpoints. Could use some work

---
+++ main.js
@@ -0,0 +1,14 @@
+let resButton = document.querySelector('#res-button')
+
+const clickButt = () => {
+    console.log("button clicked")
+}
+
+resButton.addEventListener('click', clickButt)
+
+// function clearCharacters() {
+//     while(body.firstChild){
+//         body.removeChild(body.firstChild)
+//   }
+// }
+// while(body.firstChild === 'h2')
